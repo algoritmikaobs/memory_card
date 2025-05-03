8b8d68f02e00062804f3c84d5247c8301c0e1381
algoritmikaobs <algorobs2@gmail.com>
2025-05-03 11:08:29 +0300

    Add files via upload
    v1.0

---
+++ my_memory_card.py
@@ -0,0 +1,177 @@
+#создай приложение для запоминания информации
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QRadioButton, QGroupBox, QButtonGroup
+from random import shuffle, randint
+
+
+class Question():
+    def __init__(self, question, right_answer, wrong1, wrong2, wrong3):
+        self.question = question
+        self.right_answer = right_answer
+        self.wrong1 = wrong1
+        self.wrong2 = wrong2
+        self.wrong3 = wrong3
+
+question_list = []
+question_list.append(
+    Question('Откуда пошла традиция украшать елки разноцветными шарами?',
+    'Из-за неурожая яблок', 'Из-за ошибки звездочета', 'Так приказал король', 'Просто так сложилось')
+)
+question_list.append(
+    Question('Какой из русских царей придумал праздновать Новый год зимой. 1 января?',
+    'Петр 1', 'Иван Грозный', 'Екатерина II', 'Николай II')
+)
+question_list.append(
+    Question('В какой стране самый спортивный и загорелый из Дедов Морозов приплывает к ребятам на пляж на яркой доску с парусом?',
+    'В Австралии', 'В Таиланде', 'В Египте', 'В Марокко')
+)
+question_list.append(
+    Question('В какой стране на Новый год необходимо подложить другу свинью под ёлку',
+    'В Австрии', 'В Англии', 'В Германии', 'В Финляндии')
+)
+
+app = QApplication([])
+
+# Панель вопросов
+lb_Question = QLabel('Тут вопрос')
+btn_ok = QPushButton('Ответить')
+
+RadioGroupBox = QGroupBox('Варианты ответ')     # форма вопроса
+rbtn_1 = QRadioButton('ответ 1')
+rbtn_2 = QRadioButton('ответ 2')
+rbtn_3 = QRadioButton('ответ 3')
+rbtn_4 = QRadioButton('ответ 4')
+
+RadioGroup = QButtonGroup()
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
+
+layout_ans1 = QHBoxLayout() # горизонтальная направляющая
+layout_ans2 = QVBoxLayout() # вертикальный столбец 1
+layout_ans3 = QVBoxLayout() # вертикальный столбец 2
+
+layout_ans2.addWidget(rbtn_1)
+layout_ans2.addWidget(rbtn_2)
+
+layout_ans3.addWidget(rbtn_3)
+layout_ans3.addWidget(rbtn_4)
+
+# форма с результатами
+AnsGroupBox = QGroupBox('Результаты теста')
+lb_Result = QLabel('прав ты или нет?')
+lb_Correct = QLabel('тут отобразится правильный ответ')
+
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
+
+AnsGroupBox.setLayout(layout_res)
+
+# разместим вертикальные линии на горизонтальной
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3)
+
+RadioGroupBox.setLayout(layout_ans1)        
+
+layout_line1 = QHBoxLayout()
+layout_line2 = QHBoxLayout()
+layout_line3 = QHBoxLayout()
+
+layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
+layout_line2.addWidget(RadioGroupBox)
+AnsGroupBox.hide()
+layout_line2.addWidget(AnsGroupBox)
+layout_line3.addStretch(1)
+layout_line3.addWidget(btn_ok, stretch=2)
+layout_line3.addStretch(1)
+
+layout_card = QVBoxLayout()
+
+layout_card.addLayout(layout_line1, stretch=2)
+layout_card.addLayout(layout_line2, stretch=8)
+layout_card.addStretch(1)
+layout_card.addLayout(layout_line3, stretch=1)
+layout_card.addStretch(1)
+layout_card.setSpacing(5)   # пробелы между содержимым
+
+# Функционал
+
+def show_result():
+    "спрятать варианты ответов и показать панель ответа"
+    RadioGroupBox.hide()
+    AnsGroupBox.show()
+    btn_ok.setText('Следующий вопрос')
+
+def show_question():
+    "показать панель вопросов и спрятать панель ответов"
+    RadioGroupBox.show()
+    AnsGroupBox.hide()
+    btn_ok.setText('Ответить')
+    RadioGroup.setExclusive(False)  # снять ограничение
+    rbtn_1.setChecked(False)
+    rbtn_2.setChecked(False)
+    rbtn_3.setChecked(False)
+    rbtn_4.setChecked(False)
+    RadioGroup.setExclusive(True)  # вернуть ограничение
+
+answers = [rbtn_1, rbtn_2, rbtn_3, rbtn_4]
+
+def ask(q: Question):
+    """ЗАписывает текст в соответствующие виджеты"""
+    shuffle(answers)
+    answers[0].setText(q.right_answer)
+    answers[1].setText(q.wrong1)
+    answers[2].setText(q.wrong2)
+    answers[3].setText(q.wrong3)
+    lb_Question.setText(q.question)
+    lb_Correct.setText(q.right_answer)
+    show_question()
+
+def show_correct(res):
+    lb_Result.setText(res)
+    show_result()
+
+
+def check_answer():
+    if answers[0].isChecked():
+        show_correct('Правильно!')
+        window.score += 1
+        print('Счет:', window.score, 'из', window.total)
+    else:
+        if answers[1].isChecked() or answers[2].isChecked() or answers[3].isChecked():
+            show_correct('Неверно!')
+            print('Счет:', window.score, 'из', window.total)
+
+
+def test():
+    if btn_ok.text() == 'Ответить':
+        check_answer()
+    else:
+        show_question()
+
+def show_next():
+    'Показать следующий вопрос из списка'
+    window.total += 1
+    cur_question = randint(0, len(question_list)-1)
+    q = question_list[cur_question]
+    ask(q)
+
+def click_OK():
+    if btn_ok.text() == 'Ответить':
+        check_answer()
+    else:
+        show_next()
+
+window = QWidget()
+window.total = 0
+window.score = 0
+show_next()
+btn_ok.clicked.connect(click_OK)
+window.setWindowTitle('Memo Card')
+window.setLayout(layout_card)
+
+window.resize(500, 300)
+window.show()
+app.exec()
